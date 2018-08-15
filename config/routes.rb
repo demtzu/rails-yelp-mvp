5725dab08bc798da5110cb8241716b73ce98a1b9
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-15 17:04:37 +0200

    ex completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
-
   root to: "restaurants#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
     # resources :reviews, only: [ :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
